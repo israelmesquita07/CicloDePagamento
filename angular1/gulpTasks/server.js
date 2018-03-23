86ddf717efba238554bbb5fb3e558e9cbe309c96
Israel <israel@MacBook-Pro-de-Israel.local>
2018-03-23 00:04:37 -0300

    Adicionando o Angular ao Projeto

--- angular1/gulpTasks/server.js
+++ angular1/gulpTasks/server.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp')
-const watch = require('gulp- watch')
+const watch = require('gulp-watch')
 const webserver = require('gulp-webserver')
 gulp.task('watch', () => {
     watch('app/**/*.html', () => gulp.start('app.html'))
