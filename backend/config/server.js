6ffa941c87a126c720857cb5ef3d1471c31a29b7
Israel <israel@MacBook-Pro-de-Israel.local>
2018-03-26 16:23:34 -0300

    dash front e back permission

--- backend/config/server.js
+++ backend/config/server.js
@@ -6,8 +6,11 @@ const express = require('express')
 
 const server = express()
 
+const allowCors = require('./cors')
+
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function () {
     console.log(`BackEnd está rodando na porta: ${port}.`);  
